341e99a7d7bf77c5706bbfde4706b0f5b79c0e7d
member86358@pm.me <member86358@pm.me>
2020-03-30 14:43:02 +0200

    tog bort progesssbar

--- update.ps1
+++ update.ps1
@@ -11,14 +11,9 @@ if($text -ne '' ){
     git commit -v -m "$text" 
     git push origin
     
-    for ($i = 1; $i -le 100; $i++ )
-{
-    Write-Progress -Activity "Uppladdning" -Status "$i% Klar:" -PercentComplete $i;
-    Start-Sleep -Seconds 1
-}
-if($i-eq 99){
+    Write-Progress -Activity "Uppladdning" -Status "$i% Klar:" -PercentComplete -1;
     Write-Host('Uppladdning Klar')
-}
+
 }
 else{
     Write-Host('V'+[char]::ConvertFromUtf32(0x00E4)+'nligen skriv en kommentar')
