64c0c5b5d402e9fa6615a1520837dfb4633029ed
member86358@pm.me <member86358@pm.me>
2020-03-30 14:32:17 +0200

    uppdatering update.ps1

--- update.ps1
+++ update.ps1
@@ -1,4 +1,8 @@
-git add .
+param(
+[string]$file
+)
+
+git add $file
 [void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
 convert to
 $text = [Microsoft.VisualBasic.Interaction]::InputBox($msg, "Kommentera dina " + [char]::ConvertFromUtf32(0x00C4) + "ndringar")
@@ -6,10 +10,13 @@ Write-Host($text)
 if($text -ne '' ){
     git commit -v -m "$text" 
     git push origin
-    Write-Host('Uppladdning Klar')
+    
     for ($i = 1; $i -le 100; $i++ )
 {
     Write-Progress -Activity "Uppladdning Pågår" -Status "$i% Complete:" -PercentComplete $i;
+}
+if($i-eq 100){
+    Write-Host('Uppladdning Klar')
 }
 }
 else{
