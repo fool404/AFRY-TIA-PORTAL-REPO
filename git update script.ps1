3365b0860a4b82ad12f61513720e2171fe94d9ef
member86358@pm.me <member86358@pm.me>
2020-03-30 14:11:12 +0200

    uppdatering av update.ps1 filen. hanterar om man trycker cancle eller om man inte skriver in något värde.

--- git update script.ps1
+++
@@ -1,7 +0,0 @@
-git add --verbose --update
-[void][Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
-$text = [Microsoft.VisualBasic.Interaction]::InputBox($msg, 'Kommentera utförda ändringar')
-Write-Host($text)
-git commit -m "$text"
-git push origin
-Write-Host('Done')
